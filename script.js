ad2fd621fae505b4d411497472d596ade11019b0
glebaaaasd <glebvasilukuu@gmail.com>
2026-01-12 23:45:34 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', function() {
-  const sections = document.querySelectorAll('section');
-  const scrollY = window.pageYOffset;
-  sections.forEach(section => {
-    if (scrollY + window.innerHeight > section.offsetTop + 100) {
-      section.classList.add('visible');
-    }
-  });
-});
